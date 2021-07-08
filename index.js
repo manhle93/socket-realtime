a97600d301d2e0d8b586872dd974849c9a6d70e2
ManhLe <zerokisss@gmail.com>
2021-07-08 09:50:35 +0700

    realtime cars

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-const SOCKET = require('./src/config/socket')
+const SOCKET = require("./src/config/socket");
 const express = SOCKET.express;
 const app = SOCKET.app;
 
 const port = 3000;
-const logger = require('morgan');
-const path = require('path');
+const logger = require("morgan");
+const path = require("path");
 const cors = require("cors");
-const api = require('./src/routes/api')
-
+const api = require("./src/routes/api");
+const fetch = require('node-fetch')
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 SOCKET.server.listen(port, () => {
   console.log("server start in port " + port);
 });
 SOCKET.io.on("connection", function (socket) {
   console.log("Co nguoi ket noi");
+  var realTimeCar;
+  socket.on("userconnect", function (data) {
+
+
+    realTimeCar = setInterval(() => {
+      var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+      };
+
+      fetch("http://127.0.0.1:8001/api/dinhvixe", requestOptions)
+      .then(response => response.json())
+        .then(data => {
+          socket.emit("cars", data);
+        })
+        .catch((error) => console.log("error", error));
+    }, 1000)
+  });
 });
 
-app.use('/', api);
-
-// app.post("/send", (req, res) => {
-//   io.sockets.emit("send", { name: "nads" });
-//   res.send("Da gui toi Client!");
-// });
+app.use("/", api);
--- src/controllers/NotifyController.js
+++ src/controllers/NotifyController.js
@@ -1,11 +1,11 @@
-const SOCKET = require('../config/socket')
+const SOCKET = require("../config/socket");
 
 class NotifyController {
-    sendData = (req, res, next) => {
-        const data = req.body.data
-        SOCKET.io.sockets.emit("send", data);
-        return  res.send('Gui du lieu thanh cong')
-    }
+  sendData(req, res, next) {
+    const data = req.body.data;
+    SOCKET.io.sockets.emit("send", data);
+    return res.send("Gui du lieu thanh cong");
+  }
 }
 
-module.exports = new NotifyController()
+module.exports = new NotifyController();
